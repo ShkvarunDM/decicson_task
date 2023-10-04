824c1b23ce8ed813939076831fa951852867cbe8
ShkvarunDM <103378631+ShkvarunDM@users.noreply.github.com>
2023-10-04 16:56:42 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,27 @@
+﻿
+//Напишите программу, которая читает клавиатурный ввод до символа @ и повтоторяет его, 
+//за исключанием десятичных цифр, преобразую каждую букву верхнего регистра в букву нижнего и наоборт.
+//(Не забудьте о семействе функций cctype)
+
+#include <iostream>
+#include <cctype>
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+    using std::endl;
+    char ch;
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Введите текст" << endl;
+    while ((cin >> ch) && ch != '@') {
+        if (isdigit(ch) == false) {
+            if (isalpha(ch) && isupper(ch))
+                cout << char(tolower(ch));
+            else
+                cout << char(toupper(ch));
+        }
+        else continue;
+    }
+}
