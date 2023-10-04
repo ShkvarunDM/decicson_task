f1fbb909bc00ec04b0268a59a9574eeb0b1f9483
ShkvarunDM <103378631+ShkvarunDM@users.noreply.github.com>
2023-10-04 20:27:01 +0300

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,51 @@
+﻿//Напишите программу,читающую в массив double до 10 значений пожертвований.
+// Программа должна прекращать ввод при получении нечисловой величины
+// Она должна вырадать среднее значение полученных числе, а также кол-во значений в массиве больших,чем среднее
+
+
+#include <iostream>
+#include <cctype>
+
+const int n = 10;
+
+int count_bigger_average(double, double*,int);
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+    using std::endl;
+    double sum=0;
+    int count=0;
+    setlocale(LC_ALL, "Russian");
+
+    double* array2 = new double[n];
+
+    cout << "enter number" << endl;
+    int i = 0;
+    while (i<n){
+        cin >> array2[i];
+        if (!cin) {
+            cout << "erorr number" << endl;
+            break;
+        }
+        else {
+            sum += array2[i];
+            count++;
+        }
+        i++;
+    }
+    double average = sum / count;
+    cout << "average: " << average<<endl;
+    cout << "Count bigger than average: " << count_bigger_average(average, array2, count) << endl;
+}
+
+ int count_bigger_average(double average2, double* n, int cnt) {
+    int k = 0;
+
+    for (int i = 0; i < cnt; i++) {
+        if (n[i] > average2)
+            k++;
+    }
+    return k;
+}
